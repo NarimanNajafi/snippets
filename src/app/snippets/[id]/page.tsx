b3c4f9fef6bece5f4b64901784d39c63d4adc6d7
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-03-28 19:46:02 +0330

    snippet id added

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { log } from 'console'
+import React from 'react'
+
+function snippetShowPage(props: any) {
+    console.log(props);
+
+    return (
+        <div>snippetShowPage</div>
+    )
+}
+
+export default snippetShowPage
