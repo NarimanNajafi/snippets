4d0065df9a71e626ab66cacb6123f0ba0e577330
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-03-30 19:23:50 +0330

    done

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Editor } from "@monaco-editor/react";
 import type { Snippet } from "@prisma/client";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -8,6 +9,12 @@ interface SnippetEditFormProps {
 
 
 function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+
+
+  }
   return (
     <div>
       <Editor
@@ -16,6 +23,7 @@ function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         language="javascript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   )
